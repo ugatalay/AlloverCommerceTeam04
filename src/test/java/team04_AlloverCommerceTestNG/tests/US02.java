e19d4fb0b0c4dea1a7e08c0f17a4d6acb8c29992
Siyami Eryılmaz <siyami0315@gmail.com>
2024-05-24 21:25:48 +0300

    siyami2

--- src/test/java/team04_AlloverCommerceTestNG/tests/US02.java
+++ src/test/java/team04_AlloverCommerceTestNG/tests/US02.java
@@ -3,5 +3,6 @@ package team04_AlloverCommerceTestNG.tests;
 public class US02 {
     public static void main(String[] args) {
         System.out.println("siyami");
+        System.out.println("eryılmaz");
     }
 }

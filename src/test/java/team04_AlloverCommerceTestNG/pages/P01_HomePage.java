951d50887ac0607178cacae5900ef3c894c8d7e6
ugatalay <ugratalay@gmail.com>
2024-05-25 22:43:37 +0200

    update test case 1

--- src/test/java/team04_AlloverCommerceTestNG/pages/P01_HomePage.java
+++ src/test/java/team04_AlloverCommerceTestNG/pages/P01_HomePage.java
@@ -1,4 +1,24 @@
 package team04_AlloverCommerceTestNG.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import team04_AlloverCommerceTestNG.utilities.Driver;
+
 public class P01_HomePage {
+
+    public P01_HomePage() {
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(xpath = "//span[.='Sign In']")
+    public WebElement signIn;
+
+    @FindBy(xpath = "//div[@class='search-wrapper hs-simple']//input[@name='s']")
+    public WebElement searchBox;
+
+
+
+
+
 }
--- src/test/java/team04_AlloverCommerceTestNG/tests/US06.java
+++ src/test/java/team04_AlloverCommerceTestNG/tests/US06.java
@@ -1,6 +1,8 @@
 package team04_AlloverCommerceTestNG.tests;
 
+import org.openqa.selenium.Keys;
 import org.testng.annotations.Test;
+import team04_AlloverCommerceTestNG.pages.*;
 import team04_AlloverCommerceTestNG.utilities.ConfigReader;
 import team04_AlloverCommerceTestNG.utilities.Driver;
 
@@ -9,7 +11,20 @@ public class US06 {
     @Test
     public void US06_TC01() {
 
+        P01_HomePage homePage = new P01_HomePage();
+        P03_LoginPage loginPage = new P03_LoginPage();
+
         Driver.getDriver().get(ConfigReader.getProperty("url"));
+
+        homePage.signIn.click();
+        loginPage.usernameBox.click();
+        loginPage.usernameBox.sendKeys(ConfigReader.getProperty("username"));
+        loginPage.passwordBox.sendKeys(ConfigReader.getProperty("userpassword"));
+        loginPage.signInButton.click();
+        homePage.searchBox.sendKeys(ConfigReader.getProperty("product1") + Keys.ENTER);
+
+
+
 
     }
 

497d8313007fb5a4e565895f927d42c01518d826
ugatalay <ugratalay@gmail.com>
2024-05-26 01:41:56 +0200

    us6 update

--- src/test/java/team04_AlloverCommerceTestNG/pages/P07_ShoppingPage.java
+++ src/test/java/team04_AlloverCommerceTestNG/pages/P07_ShoppingPage.java
@@ -1,6 +1,5 @@
 package team04_AlloverCommerceTestNG.pages;
 
-public class P07_ShoppingPage {
-
+public class P07_ShoppingPage{
 
 }
--- src/test/java/team04_AlloverCommerceTestNG/tests/US06.java
+++ src/test/java/team04_AlloverCommerceTestNG/tests/US06.java
@@ -3,28 +3,25 @@ package team04_AlloverCommerceTestNG.tests;
 import org.openqa.selenium.Keys;
 import org.testng.annotations.Test;
 import team04_AlloverCommerceTestNG.pages.*;
-import team04_AlloverCommerceTestNG.utilities.ConfigReader;
-import team04_AlloverCommerceTestNG.utilities.Driver;
+import team04_AlloverCommerceTestNG.utilities.*;
 
 public class US06 {
 
     @Test
-    public void US06_TC01() {
+    public void US06_TC01() throws InterruptedException {
 
-        P01_HomePage homePage = new P01_HomePage();
-        P03_LoginPage loginPage = new P03_LoginPage();
+        P00_MainPage mainPage = new P00_MainPage();
 
         Driver.getDriver().get(ConfigReader.getProperty("url"));
 
-        homePage.signIn.click();
-        loginPage.usernameBox.click();
-        loginPage.usernameBox.sendKeys(ConfigReader.getProperty("username"));
-        loginPage.passwordBox.sendKeys(ConfigReader.getProperty("userpassword"));
-        loginPage.signInButton.click();
-        homePage.searchBox.sendKeys(ConfigReader.getProperty("product1") + Keys.ENTER);
-
-
-
+        mainPage.homePage().signIn.click();
+        mainPage.userVendorLoginPage().usernameBox.click();
+        mainPage.userVendorLoginPage().usernameBox.sendKeys(ConfigReader.getProperty("username"));
+        mainPage.userVendorLoginPage().passwordBox.sendKeys(ConfigReader.getProperty("userpassword"));
+        mainPage.userVendorLoginPage().signInButton.click();
+        ReusableMethods.waitForSecond(3);
+        mainPage.homePage().searchBox.sendKeys(ConfigReader.getProperty("product1")+ Keys.ENTER);
+        ReusableMethods.waitForSecond(3);
 
     }
 

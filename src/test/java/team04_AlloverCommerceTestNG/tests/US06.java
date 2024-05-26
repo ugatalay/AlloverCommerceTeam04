0bdf657c8f85e43c8bc2dbc83ab2c1fb6e76ed2c
ugatalay <ugratalay@gmail.com>
2024-05-26 15:45:57 +0200

    login method added

--- src/test/java/team04_AlloverCommerceTestNG/tests/US06.java
+++ src/test/java/team04_AlloverCommerceTestNG/tests/US06.java
@@ -12,15 +12,12 @@ public class US06 {
     public void US06_TC01() throws InterruptedException {
 
         P00_MainPage mainPage = new P00_MainPage();
+        mainPage.userVendorLoginPage().login();
 
-        Driver.getDriver().get(ConfigReader.getProperty("url"));
 
-        mainPage.homePage().signIn.click();
-        mainPage.userVendorLoginPage().usernameBox.click();
-        mainPage.userVendorLoginPage().usernameBox.sendKeys(ConfigReader.getProperty("username"));
-        mainPage.userVendorLoginPage().passwordBox.sendKeys(ConfigReader.getProperty("userpassword"));
-        mainPage.userVendorLoginPage().signInButton.click();
-        ReusableMethods.waitForSecond(3);
+
+
+
 
         mainPage.homePage().searchBox.sendKeys(ConfigReader.getProperty("product1")+ Keys.ENTER);
         ReusableMethods.waitForSecond(3);

0cb36409bbef9dedd921cfe90e69612cb3740a14
ugatalay <ugratalay@gmail.com>
2024-05-26 22:55:44 +0200

    test case 2 added

--- src/test/java/team04_AlloverCommerceTestNG/pages/P07_ShoppingPage.java
+++ src/test/java/team04_AlloverCommerceTestNG/pages/P07_ShoppingPage.java
@@ -12,13 +12,23 @@ public class P07_ShoppingPage{
         PageFactory.initElements(Driver.getDriver(),this);
     }
 
+    P00_MainPage mainPage = new P00_MainPage();
+
     @FindBy(xpath = "//h1[@class='product_title entry-title']")
     public WebElement samsungS23;
 
+    @FindBy(xpath = "//p[@class='alert alert-light alert-info woocommerce-info']")
+    public WebElement searchResult;
+
     //Methods
-    P00_MainPage mainPage = new P00_MainPage();
+
     public void searchProduct() {
         mainPage.homePage().searchBox.sendKeys(ConfigReader.getProperty("product1")+ Keys.ENTER);
+        ReusableMethods.waitForSecond(3);
+    }
+
+    public void invalidSearch() {
+        mainPage.homePage().searchBox.sendKeys(ConfigReader.getProperty("invalid_search") + Keys.ENTER);
         ReusableMethods.waitForSecond(3);
     }
 
--- src/test/java/team04_AlloverCommerceTestNG/tests/US06.java
+++ src/test/java/team04_AlloverCommerceTestNG/tests/US06.java
@@ -6,15 +6,20 @@ import team04_AlloverCommerceTestNG.pages.*;
 
 public class US06 {
 
+    P00_MainPage mainPage = new P00_MainPage();
+
     @Test
     public void US06_TC01() throws InterruptedException {
-
-        P00_MainPage mainPage = new P00_MainPage();
         mainPage.userVendorLoginPage().login();
         mainPage.shoppingPage().searchProduct();
         Assert.assertTrue(mainPage.shoppingPage().samsungS23.isDisplayed());
 
     }
 
-
+    @Test
+    public void US06_TC02() {
+        mainPage.userVendorLoginPage().login();
+        mainPage.shoppingPage().invalidSearch();
+        Assert.assertEquals("No products were found matching your selection.", mainPage.shoppingPage().searchResult.getText());
+    }
 }

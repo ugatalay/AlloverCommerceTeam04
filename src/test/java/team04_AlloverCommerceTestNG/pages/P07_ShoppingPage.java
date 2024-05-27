7c4eaee80dfa529822bcaa3f9298d19d5003138c
Mukaddes KUCUK <mukaddeszorlu@gmail.com>
2024-05-27 21:33:06 +0200

    guncelleme

--- src/test/java/team04_AlloverCommerceTestNG/pages/P07_ShoppingPage.java
+++ src/test/java/team04_AlloverCommerceTestNG/pages/P07_ShoppingPage.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import team04_AlloverCommerceTestNG.utilities.*;
 
-
+public class P07_ShoppingPage {
     public P07_ShoppingPage() {
         PageFactory.initElements(Driver.getDriver(),this);
     }

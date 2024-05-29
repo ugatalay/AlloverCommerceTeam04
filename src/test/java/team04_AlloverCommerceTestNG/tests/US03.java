ca0e1ce1b5887e5044ace573170f3e04d9de31d2
Siyami Eryılmaz <siyami0315@gmail.com>
2024-05-29 16:57:32 +0300

    deneme1

--- src/test/java/team04_AlloverCommerceTestNG/tests/US03.java
+++ src/test/java/team04_AlloverCommerceTestNG/tests/US03.java
@@ -4,6 +4,8 @@ import org.apache.logging.log4j.core.util.JsonUtils;
 
 public class US03 {
     public static void main(String[] args) {
+
         System.out.println("selam");
+        System.out.println("deneme");
     }
 }
